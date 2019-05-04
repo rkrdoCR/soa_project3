a153d8d9c8424257ea3d97d135f41cc522291d82
Christopher Jimenez <cjimenez2581@gmail.com>
2019-05-04 08:37:07 -0600

    implementation without arrays

--- data_structures/settings.c
+++ data_structures/settings.c
@@ -3,6 +3,11 @@
 settings* fillSettings(int schedulable,int combineAll,int rm, int edf, int llf){
     printf("Started filling");
     settings* formSettings = malloc(sizeof(settings));
+    formSettings->schedulable = schedulable;
+    formSettings->combineAll = combineAll;
+    formSettings->rm = rm;
+    formSettings->edf = edf;
+    formSettings->llf = llf;
 
     return formSettings;
 }
