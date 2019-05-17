63f453ee24ef7abd950a8eaf7c087bb1d5d549d5
rjimenez167 <rjimenez167@gmail.com>
2019-05-17 16:25:05 -0600

    Add files via upload

--- data_structures/settings.h
+++ data_structures/settings.h
@@ -7,12 +7,14 @@ typedef struct
 {
     int executionTimes[6];
     int periods[6];
+    int MCD;
     //algorithms
     int rm;
     int edf;
     int llf;
     // checkboxes 
     int combineAll;
+
 
 }simulation_settings;
 
