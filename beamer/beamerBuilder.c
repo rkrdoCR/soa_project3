6d9c3812786f1a0c87885653698e88c5c3ee704b
Marco <marcav92@hotmail.com>
2019-05-26 14:21:57 -0600

    Beamer is already being nerated nicely, yet I havent added tests results, there are still several segmentation fault errors with several task sets

--- beamer/beamerBuilder.c
+++ beamer/beamerBuilder.c
@@ -1,6 +1,10 @@
 //
 #include "beamerBuilder.h"
 
+int decoder_index=0;
+char decoder_keys[100][100];
+char decoder_values[100][100];
+
 
 void removeNewLine(char *token, int tokenLength){
     for(int i=0; i<tokenLength;i++){
@@ -44,7 +48,7 @@ void copySnippet(char *token, char *snippet){
 void clean_buffer(char *buffer, int length){
     int i=0;
     for(i=0;i<length;i++){
-        buffer[i]="";
+        buffer[i]="\0";
     }
 }
 
@@ -82,10 +86,12 @@ int readTemplate(){
                 
                 removeNewLine(token,200);
                 printf("Im about to get to copysnippet\n");
+                StringDecoder(token,buffer);
                 copySnippet(token,buffer);
                 fseek(target,-1,SEEK_CUR);
                 fprintf(target, "%s", buffer);
-                clean_buffer(buffer,2000);
+                //clean_buffer(buffer,2000);
+                memset(buffer, 0, 2000);
             }else{
                 fputc(ch, target);
             }
@@ -103,13 +109,29 @@ int readTemplate(){
 }
 
 
-int copyTemplateToTempBash(){
-    int status = system("cp assets/* tmpfiles");
-    if (status == 0){
-        printf("Template copy was made successfully \n");
-    }else{
-        printf("Something went wrong in copy of template");
-    }
+// int copyTemplateToTempBash(){
+//     int status = system("cp assets/* tmpfiles");
+//     if (status == 0){
+//         printf("Template copy was made successfully \n");
+//     }else{
+//         printf("Something went wrong in copy of template");
+//     }
+// }
+
+int copyTemplateToTempBash(char *file_to_copy){
+   char full_command[150];
+   printf("Im inside copy template and this is file to copy: \n%s",file_to_copy);
+   strcpy(full_command,"cp assets/");
+   strcat(full_command,file_to_copy);
+   strcat(full_command," tmpfiles");
+   printf("Im inside copy template and this is full_command: \n%s",full_command);
+
+   int status = system(full_command);
+   if (status == 0){
+       printf("Template copy was made successfully \n");
+   }else{
+       printf("Something went wrong in copy of template");
+   }
 }
 
 
@@ -117,6 +139,31 @@ void compileBeamer(){
     system("pdflatex -interaction nonstopmode -output-directory tmpfiles/ tmpfiles/template_final.tex >/dev/null");
     system("evince tmpfiles/template_final.pdf &");
 }
+
+void StringDecoder(char *input, char *output){
+    int i=0;
+    int c=100;
+    for (i=0; i<decoder_index;i++){
+        c=strcmp(decoder_keys[i],input);
+        if(c==0){
+            strcpy(output,decoder_values[i]);
+        }
+    }
+}
+
+void addKeyToDecoder(char *key, char *value){
+    strcpy(decoder_keys[decoder_index],key);
+    strcpy(decoder_values[decoder_index],value);
+    decoder_index++;
+}
+
+void flushDecoder(){
+    int i=0;
+    for (i=0; i<100;i++){
+        memset(decoder_keys[i],0,100);
+    }
+}
+
 
 
 void createTabular(algo_results *ar, int tasks_count, int lcm, char *tabular_name){
@@ -127,12 +174,24 @@ void createTabular(algo_results *ar, int tasks_count, int lcm, char *tabular_nam
 
     target=fopen(full_path, "w");
     char buffer[1000]="";
-    int i,j=0;
+    int i,j,k=0;
     char str_i[100];
+    char title_name[100];
 
     printf("Creating this tabular: %s", tabular_name);
 
     strcat(buffer, "\\begin{table}[]\n");
+
+    //table title
+    //strcat(buffer,"\\caption {Table Title} \\label{tab:title}");
+    strcat(buffer,"\\caption {");
+    StringDecoder(tabular_name,title_name);
+    strcat(buffer,title_name);
+    strcat(buffer,"} \\label{tab:");
+    strcat(buffer,tabular_name);
+    strcat(buffer,"}\n");
+
+
     strcat(buffer, "\\small\\addtolength{\\tabcolsep}{-5pt}\n");
     strcat(buffer, "\\begin{tabular}");
     strcat(buffer, "{");
@@ -209,26 +268,48 @@ void createTabular(algo_results *ar, int tasks_count, int lcm, char *tabular_nam
     printf("file closed successfully");
 }
 
+
 void createBeamer(algo_results *ar, int tasks_count, int lcm){
     char tabular_name[10];
-    copyTemplateToTempBash();
+    flushTempFiles();
+    flushDecoder();
+    copyTemplateToTempBash("template.tex");
+
 
     if(ar[0].selected==1){
-        strcpy(tabular_name,"tabular1");
-        printf("This is tabular name: %s", tabular_name);
-        createTabular(&ar[0], tasks_count, lcm, tabular_name);
+        addKeyToDecoder("subsectionrm","\\subsection{Rate Monothonic}");
+        addKeyToDecoder("tabular1","Resultados de simulacion de Rate Monothonic");
+        addKeyToDecoder("beginrmd","\\begin{frame}{Descripcion de Rate Monothonic}\n");
+        addKeyToDecoder("beginrmt","\\begin{frame}{Resultados test de Rate Monothonic}\n");
+        addKeyToDecoder("beginrms","\\begin{frame}{Resultados sim de Rate Monothonic}\n");
+        addKeyToDecoder("endrm","\\end{frame}\n");
+        copyTemplateToTempBash("description11");
+        copyTemplateToTempBash("description12");
+        createTabular(&ar[0], tasks_count, lcm, "tabular1");
     }
     
     if(ar[1].selected==1){
-        strcpy(tabular_name,"tabular2");
-        printf("This is tabular name: %s", tabular_name);
-        createTabular(&ar[1], tasks_count, lcm, tabular_name);
+        addKeyToDecoder("subsectionedf","\\subsection{Earliest deadline first}");
+        addKeyToDecoder("tabular2","Resultados de simulacion de Earliest Deadline first");
+        addKeyToDecoder("beginedfd","\\begin{frame}{Descripcion de Earliest Deadline first}\n");
+        addKeyToDecoder("beginedft","\\begin{frame}{Resultados test de Earliest Deadline first}\n");
+        addKeyToDecoder("beginedfs","\\begin{frame}{Resultados sim de Earliest Deadline first}\n");
+        addKeyToDecoder("endedf","\\end{frame}\n");
+        copyTemplateToTempBash("description21");
+        copyTemplateToTempBash("description22");
+        createTabular(&ar[1], tasks_count, lcm, "tabular2");
     }
 
     if(ar[2].selected==1){
-        strcpy(tabular_name,"tabular3");
-        printf("This is tabular name: %s", tabular_name);
-        createTabular(&ar[2], tasks_count, lcm, tabular_name);
+        addKeyToDecoder("subsectionllf","\\subsection{Least Laxity first}");
+        addKeyToDecoder("tabular3","Resultados de simulacion de Least Laxity first");
+        addKeyToDecoder("beginllfd","\\begin{frame}{Descripcion de Least Laxity First}\n");
+        addKeyToDecoder("beginllft","\\begin{frame}{Resultados test de Least Laxity First}\n");
+        addKeyToDecoder("beginllfs","\\begin{frame}{Resultados sim de Least Laxity First}\n");
+        addKeyToDecoder("endllf","\\end{frame}\n");
+        copyTemplateToTempBash("description31");
+        copyTemplateToTempBash("description32");
+        createTabular(&ar[2], tasks_count, lcm, "tabular3");
     }
 
     readTemplate();
@@ -237,5 +318,16 @@ void createBeamer(algo_results *ar, int tasks_count, int lcm){
 
     
 };
+
+void flushTempFiles(){
+    int status = system("rm tmpfiles/*");
+    if (status == 0){
+        printf("tmpFiles cleared\n");
+    }else{
+       printf("Something went wrong clearing temp files");
+    }
+}
+
+
 
 
--- beamer/beamerBuilder.h
+++ beamer/beamerBuilder.h
@@ -7,12 +7,16 @@
 
 #include "../data_structures/algo_results.h"
 
-int copyTemplateToTempBash();
+//int copyTemplateToTempBash();
+int copyTemplateToTempBash(char *file_to_copy);
 int copyTemplateToTemp();
 int readTemplate();
 void createBeamer(algo_results *ar, int tasks_count, int lcm);
 void compileBeamer();
 void createTabular(algo_results *ar, int tasks_count, int lcm, char *tabular_name);
-
+void flushTempFiles();
+void StringDecoder(char *input, char *output);
+void addKeyToDecoder(char *key, char *value);
+void flushDecoder();
 
 #endif
